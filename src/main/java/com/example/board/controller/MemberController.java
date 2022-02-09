9ffdc418565ae7131d0645c76c0a0f8850b9c0ef
lilpard <nadanwi7@gmail.com>
2022-02-09 16:17:32 +0900

    Update MemberController.java

--- src/main/java/com/example/board/controller/MemberController.java
+++ src/main/java/com/example/board/controller/MemberController.java
@@ -22,16 +22,19 @@ public class MemberController {
 	@Autowired
 	MemberService service;
 	
+	// 아이디 중복 확인
 	@PostMapping("api/member/idCheck")
 	public Map<String, Object> idCheck(@RequestParam(value = "id") String id){
 		return service.idCheck(id);
 	}
 	
+	// 회원 가입
 	@PostMapping(value =  "api/member", produces = "application/json; charset=UTF-8")
 	public Map<String, Object> join(@RequestBody Member member){
 		return service.addMember(member);
 	}
 	
+	// 로그인
 	@PostMapping(value = "api/member/login", produces = "application/json; charset=UTF-8")
 	public Map<String, Object> login(@RequestBody Member member, HttpServletRequest request, Model model){
 		model.addAttribute("member", member);
@@ -40,6 +43,7 @@ public class MemberController {
 		return service.loginMember(model);
 	}
 	
+	// 회원 정보 수정
 	@PutMapping(value = "api/member", produces = "application/json; charset=UTF-8")
 	public Map<String, Object> modifyMember(@RequestBody Member member, HttpServletRequest request, Model model){
 		model.addAttribute("member", member);
@@ -48,6 +52,7 @@ public class MemberController {
 		return service.modifyMember(model);
 	}
 	
+	// 회원 탈퇴
 	@DeleteMapping(value = "api/member/{mno}")
 	public Map<String, Object> deleteMember(@PathVariable (value = "mno") Long mno, HttpServletRequest request, Model model){
 		model.addAttribute("mno", mno);
